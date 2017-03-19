a03da3e6bf25c53fa9fb9129b23a0e6c67ae728d
John Papa <john@johnpapa.net>
2017-03-19 10:10:24 -0400

    back to using res separately

--- characters/index.js
+++ characters/index.js
@@ -14,13 +14,13 @@ module.exports = function (context, req) {
         const id = parseInt(req.query.id);
         const customer = customers.filter(c => c.id === id);
         // context.log(req.query.id);
-        context.res = {
+        res = {
             // status: 200, /* Defaults to 200 */
             body: customer
         };
     }
     else {
-        context.res = {
+        res = {
             // status: 200, /* Defaults to 200 */
             body: customers
         };
@@ -29,6 +29,6 @@ module.exports = function (context, req) {
         //     body: "Please pass a name on the query string or in the request body"
         // };
     }
-    //context.done(null, res);
-    context.done();
+    context.done(null, res);
+    // context.done();
 };
