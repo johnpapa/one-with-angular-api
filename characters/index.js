5761fa3318d59f0d4947527e332e66ff4210a408
John Papa <john@Johns-MacBook-Pro.local>
2017-03-29 11:58:24 -0400

    dude

--- characters/index.js
+++ characters/index.js
@@ -6,7 +6,8 @@ module.exports = function (context, req) {
     const customers = [
         { id: 1, name: 'John', city: 'Orlando' },
         { id: 2, name: 'Peggy', city: 'New York' },
-        { id: 3, name: 'Julie', city: 'Seattle' }
+        { id: 3, name: 'Julie', city: 'Seattle' },
+        { id: 4, name: 'Brian', city: 'New York' }
     ];
 
     // if (req.query.name || (req.body && req.body.name)) {
